28fb23a6ccb4ae3b2816d3ee7a948babd029daa7
Leo Romanovsky <leo@geteppo.com>
2024-02-26 09:53:44 -0800

    add unit tests for semver conditions

--- src/rule_evaluator.spec.ts
+++ src/rule_evaluator.spec.ts
@@ -69,6 +69,120 @@ describe('findMatchingRule', () => {
     expect(findMatchingRule({ version: '2.1.0' }, rules, false)).toBeNull();
   });
 
+  it('returns the rule for semver prerelease conditions', () => {
+    /*
+    https://semver.org/#spec-item-9
+
+    A pre-release version MAY be denoted by appending a hyphen and a series of dot separated identifiers immediately following the patch version. 
+    Identifiers MUST comprise only ASCII alphanumerics and hyphens [0-9A-Za-z-]. Identifiers MUST NOT be empty. 
+    Numeric identifiers MUST NOT include leading zeroes. Pre-release versions have a lower precedence than the associated normal version. 
+    A pre-release version indicates that the version is unstable and might not satisfy the intended compatibility requirements as denoted by its associated normal version. 
+    Examples: 1.0.0-alpha, 1.0.0-alpha.1, 1.0.0-0.3.7, 1.0.0-x.7.z.92, 1.0.0-x-y-z.--.
+
+    Pre-release versions have a lower precedence than the associated normal version.
+    For example, 1.0.0-alpha < 1.0.0.
+    */
+    const extendedSemverRule: IRule = {
+      allocationKey: 'test',
+      conditions: [
+        {
+          operator: OperatorType.GT,
+          attribute: 'version',
+          value: '1.2.3-alpha',
+        },
+        {
+          operator: OperatorType.LTE,
+          attribute: 'version',
+          value: '2.0.0',
+        },
+      ],
+    };
+    const rules = [extendedSemverRule];
+
+    // is greater than the associated alpha version
+    expect(findMatchingRule({ version: '1.2.3' }, rules, false)).toEqual(extendedSemverRule);
+
+    // beta is greater than alpha (lexicographically)
+    expect(findMatchingRule({ version: '1.2.3-beta' }, rules, false)).toEqual(extendedSemverRule);
+
+    // 1.2.4 is greater than 1.2.3
+    expect(findMatchingRule({ version: '1.2.4' }, rules, false)).toEqual(extendedSemverRule);
+  });
+
+  it('returns the rule for semver build numbers', () => {
+    /*
+    https://semver.org/#spec-item-10
+
+    Build metadata MAY be denoted by appending a plus sign and a series of dot separated identifiers immediately following the patch or pre-release version. 
+    Identifiers MUST comprise only ASCII alphanumerics and hyphens [0-9A-Za-z-]. Identifiers MUST NOT be empty. 
+    Build metadata MUST be ignored when determining version precedence. 
+    Thus two versions that differ only in the build metadata, have the same precedence. 
+    
+    Examples: 1.0.0-alpha+001, 1.0.0+20130313144700, 1.0.0-beta+exp.sha.5114f85, 1.0.0+21AF26D3----117B344092BD.
+    */
+
+    const extendedSemverRule: IRule = {
+      allocationKey: 'test',
+      conditions: [
+        {
+          operator: OperatorType.GT,
+          attribute: 'version',
+          value: '1.2.3+001',
+        },
+        {
+          operator: OperatorType.LTE,
+          attribute: 'version',
+          value: '2.0.0',
+        },
+      ],
+    };
+    const rules = [extendedSemverRule];
+
+    // build number is ignored therefore 1.2.3 is not greater than 1.2.3
+    expect(findMatchingRule({ version: '1.2.3' }, rules, false)).toEqual(null);
+
+    // build number is ignored therefore 1.2.3 is not greater than 1.2.3
+    expect(findMatchingRule({ version: '1.2.3+500' }, rules, false)).toEqual(null);
+
+    // 1.2.4 is greater than 1.2.3
+    expect(findMatchingRule({ version: '1.2.4' }, rules, false)).toEqual(extendedSemverRule);
+  });
+
+  it('returns the rule for semver mixed prerelease and build numbers', () => {
+    /*
+    When a version in Semantic Versioning (SemVer) includes both pre-release identifiers and build metadata, 
+    the version's precedence is determined first by the pre-release version, 
+    with the build metadata being ignored in terms of precedence. 
+    
+    The format for such a version would look something like this: MAJOR.MINOR.PATCH-prerelease+build
+    */
+    const extendedSemverRule: IRule = {
+      allocationKey: 'test',
+      conditions: [
+        {
+          operator: OperatorType.GT,
+          attribute: 'version',
+          value: '1.2.3-beta+001',
+        },
+        {
+          operator: OperatorType.LTE,
+          attribute: 'version',
+          value: '2.0.0',
+        },
+      ],
+    };
+    const rules = [extendedSemverRule];
+
+    // gamma is greater than beta (lexicographically)
+    expect(findMatchingRule({ version: '1.2.3-gamma' }, rules, false)).toEqual(extendedSemverRule);
+
+    // 1.2.3 is greater than 1.2.3-beta; this is a stable release
+    expect(findMatchingRule({ version: '1.2.3+500' }, rules, false)).toEqual(extendedSemverRule);
+
+    // 1.2.4 is greater than 1.2.3
+    expect(findMatchingRule({ version: '1.2.4' }, rules, false)).toEqual(extendedSemverRule);
+  });
+
   it('returns null if there is no attribute for the condition', () => {
     const rules = [numericRule];
     expect(findMatchingRule({ unknown: 'test' }, rules, false)).toEqual(null);
