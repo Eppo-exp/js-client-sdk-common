bb1d2e196cbb68b54f66a43ba55cc37ceb1fdd01
Sven Schmit <sven@geteppo.com>
2024-03-07 15:35:06 -0800

    formatting

--- src/obfuscation.spec.ts
+++ src/obfuscation.spec.ts
@@ -10,8 +10,13 @@ describe('obfuscation', () => {
   });
 
   it('encodes/decodes regex', () => {
-    const regexes = ['.*@example.com', '.*@.*.com', '\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b',
-      '^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$', '\b(?:\d{1,3}\.){3}\d{1,3}\b', '\b\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])\b', '\b(?:\d[ -]*?){13,16}\b'];
+    const regexes = [
+      '.*@example.com',
+      '.*@.*.com',
+      '\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b',
+      '^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$', '\b(?:\d{1,3}\.){3}\d{1,3}\b',
+      '\b\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])\b', '\b(?:\d[ -]*?){13,16}\b'
+    ];
 
     regexes.forEach((regex) => {
       expect(decodeBase64(encodeBase64(regex))).toEqual(regex);
