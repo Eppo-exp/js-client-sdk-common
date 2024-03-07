e9efba17d7d3e45a93dc5767a38a60bd98c8188a
Sven Schmit <sven@geteppo.com>
2024-03-07 15:33:02 -0800

    Update encoding to remove Buffer

--- src/obfuscation.spec.ts
+++ src/obfuscation.spec.ts
@@ -8,4 +8,13 @@ describe('obfuscation', () => {
   it('decodes base64 to string', () => {
     expect(Number(decodeBase64('NS4w'))).toEqual(5);
   });
+
+  it('encodes/decodes regex', () => {
+    const regexes = ['.*@example.com', '.*@.*.com', '\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b',
+      '^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$', '\b(?:\d{1,3}\.){3}\d{1,3}\b', '\b\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])\b', '\b(?:\d[ -]*?){13,16}\b'];
+
+    regexes.forEach((regex) => {
+      expect(decodeBase64(encodeBase64(regex))).toEqual(regex);
+    }
+  });
 });
--- src/obfuscation.ts
+++ src/obfuscation.ts
@@ -1,13 +1,31 @@
 import * as md5 from 'md5';
 
+
 export function getMD5Hash(input: string): string {
   return md5(input);
 }
 
+function base64ToBytes(base64: string) {
+  const binaryString = atob(base64);
+  const bytes = new Uint8Array(binaryString.length);
+  for (let i = 0; i < binaryString.length; i++) {
+    bytes[i] = binaryString.charCodeAt(i);
+  }
+  return bytes;
+}
+
+function bytesToBase64(bytes: Uint8Array) {
+  const binString = Array.from(bytes, (byte) =>
+    String.fromCodePoint(byte),
+  ).join("");
+  return btoa(binString);
+}
+
 export function encodeBase64(input: string): string {
-  return Buffer.from(input, 'utf8').toString('base64');
+  return bytesToBase64(new TextEncoder().encode(input));
 }
 
 export function decodeBase64(input: string): string {
-  return Buffer.from(input, 'base64').toString('utf8');
+  return new TextDecoder().decode(base64ToBytes(input))
+
 }
