b9db61ab5a7aeeca8e73a3ee2e5c874c7ae4446a
Leo Romanovsky <leo@geteppo.com>
2024-05-03 16:01:06 -0700

    add pino pretty and prefix logs with `[Eppo SDK]

--- src/application-logger.ts
+++ src/application-logger.ts
@@ -1,8 +1,18 @@
 import pino from 'pino';
+import pretty from 'pino-pretty';
+
+const prettyStream = pretty({
+  translateTime: true,
+  ignore: 'pid,hostname',
+  messageFormat: '[Eppo SDK] {msg}',
+});
 
 // Create a Pino logger instance
-export const logger = pino({
-  level: process.env.NODE_ENV === 'production' ? 'warn' : 'info',
-  // https://getpino.io/#/docs/browser
-  browser: { disabled: true },
-});
+export const logger = pino(
+  {
+    level: process.env.NODE_ENV === 'production' ? 'warn' : 'info',
+    // https://getpino.io/#/docs/browser
+    browser: { disabled: true },
+  },
+  prettyStream,
+);
