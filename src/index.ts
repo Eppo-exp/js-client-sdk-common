ac4d42029769780bc744cc9aceffafa963e70fb9
Leo Romanovsky <leoromanovsky@users.noreply.github.com>
2024-05-09 19:23:43 -0500

    export configuration store interfaces (#63)

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,13 @@ import { AssignmentCache } from './assignment-cache';
 import { IAssignmentHooks } from './assignment-hooks';
 import { IAssignmentLogger, IAssignmentEvent } from './assignment-logger';
 import EppoClient, { FlagConfigurationRequestParameters, IEppoClient } from './client/eppo-client';
-import { IConfigurationStore } from './configuration-store/configuration-store';
+import {
+  IConfigurationStore,
+  IAsyncStore,
+  ISyncStore,
+} from './configuration-store/configuration-store';
 import { HybridConfigurationStore } from './configuration-store/hybrid.store';
+import { MemoryOnlyConfigurationStore } from './configuration-store/memory.store';
 import * as constants from './constants';
 import FlagConfigRequestor from './flag-configuration-requestor';
 import HttpClient from './http-client';
@@ -23,8 +28,15 @@ export {
   FlagConfigRequestor,
   HttpClient,
   validation,
+
+  // Configuration store
   IConfigurationStore,
+  IAsyncStore,
+  ISyncStore,
   HybridConfigurationStore,
+  MemoryOnlyConfigurationStore,
+
+  //
   AssignmentCache,
   FlagConfigurationRequestParameters,
   Flag,
