9e5be9e9f5b578134f9ba55a20c2927aa778cbec
Greg Huels <greg.huels@geteppo.com>
2024-06-03 13:08:19 -0500

    FF-2283 determine enable client-side logging when "widnow" is available

--- src/application-logger.ts
+++ src/application-logger.ts
@@ -2,9 +2,11 @@ import pino from 'pino';
 
 export const loggerPrefix = '[Eppo SDK]';
 
+const isNode = typeof window === 'undefined';
+
 // Create a Pino logger instance
 export const logger = pino({
   level: process.env.NODE_ENV === 'production' ? 'warn' : 'info',
   // https://getpino.io/#/docs/browser
-  browser: { disabled: true },
+  browser: { disabled: isNode },
 });
