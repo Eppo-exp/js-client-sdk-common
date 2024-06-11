40c42f7be0ef0c2d387341636434bb748bda670d
Aaron Silverman <aaron.silverman@geteppo.com>
2024-06-11 09:24:08 -0600

    just generate normal source maps

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 
-const CopyPlugin = require('copy-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
   mode: 'production',
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
   target: 'web',
   module: {
     rules: [
@@ -33,18 +32,4 @@ module.exports = {
     minimize: true,
     minimizer: [new TerserPlugin()],
   },
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        {
-          from: 'src/**/*.ts',
-          to: path.resolve(__dirname, 'dist'),
-          noErrorOnMissing: true,
-          globOptions: {
-            ignore: ['**/*.spec.ts'], // Exclude test and spec files
-          },
-        },
-      ],
-    }),
-  ],
 };
