254ab44b75dd165f678495eb9c2e50f3f8ea7bcd
Sameeran Kunche <skunche@gmail.com>
2024-06-20 15:54:53 -0700

    Rename interface

--- src/api-endpoints.ts
+++ src/api-endpoints.ts
@@ -1,14 +1,14 @@
 import { BASE_URL as DEFAULT_BASE_URL, UFC_ENDPOINT } from './constants';
 import { ISdkParams } from './http-client';
 
-interface IApiEndpointParams {
+interface IApiEndpointsParams {
   queryParams: ISdkParams;
   baseUrl?: string;
 }
 
 /** Utility class for constructing an Eppo API endpoint URL given a provided baseUrl and query parameters */
 export default class ApiEndpoints {
-  constructor(private readonly params: IApiEndpointParams) {
+  constructor(private readonly params: IApiEndpointsParams) {
     this.params.baseUrl = params.baseUrl ?? DEFAULT_BASE_URL;
   }
 
