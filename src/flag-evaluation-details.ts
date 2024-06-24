ddadd28fdefe90dc50ba33d2e6c26443344ac684
Greg Huels <greg.huels@gmail.com>
2024-06-24 07:14:35 -0500

    FF-2498 Evaluation Details, excluding config fetch/publish time (#99)
    * FF-2465 New "Details" methods for "Evaluation Reasons" metadata
    
    * FF-2498 Include "unevaluated" allocations

---
+++ src/flag-evaluation-details.ts
@@ -0,0 +1,127 @@
+import { Allocation, Variation } from './interfaces';
+import { Rule } from './rules';
+
+export const flagEvaluationCodes = [
+  'MATCH',
+  'FLAG_UNRECOGNIZED_OR_DISABLED',
+  'TYPE_MISMATCH',
+  'ASSIGNMENT_ERROR',
+  'DEFAULT_ALLOCATION_NULL',
+] as const;
+
+export type FlagEvaluationCode = typeof flagEvaluationCodes[number];
+
+export enum AllocationEvaluationCode {
+  UNEVALUATED = 'UNEVALUATED',
+  MATCH = 'MATCH',
+  BEFORE_START_TIME = 'BEFORE_START_TIME',
+  TRAFFIC_EXPOSURE_MISS = 'TRAFFIC_EXPOSURE_MISS',
+  AFTER_END_TIME = 'AFTER_END_TIME',
+  FAILING_RULE = 'FAILING_RULE',
+}
+
+export interface AllocationEvaluation {
+  key: string;
+  allocationEvaluationCode: AllocationEvaluationCode;
+  orderPosition: number;
+}
+
+export interface FlagEvaluationDetails {
+  variationKey: string | null;
+  variationValue: Variation['value'] | null;
+  flagEvaluationCode: FlagEvaluationCode;
+  flagEvaluationDescription: string;
+  matchedRule: Rule | null;
+  matchedAllocation: AllocationEvaluation | null;
+  unmatchedAllocations: Array<AllocationEvaluation>;
+  unevaluatedAllocations: Array<AllocationEvaluation>;
+}
+
+export class FlagEvaluationDetailsBuilder {
+  private variationKey: FlagEvaluationDetails['variationKey'];
+  private variationValue: FlagEvaluationDetails['variationValue'];
+  private matchedRule: FlagEvaluationDetails['matchedRule'];
+  private matchedAllocation: FlagEvaluationDetails['matchedAllocation'];
+  private unmatchedAllocations: FlagEvaluationDetails['unmatchedAllocations'];
+  private unevaluatedAllocations: FlagEvaluationDetails['unevaluatedAllocations'];
+
+  constructor(private readonly allocations: Allocation[]) {
+    this.setNone();
+  }
+
+  setNone = (): FlagEvaluationDetailsBuilder => {
+    this.variationKey = null;
+    this.variationValue = null;
+    this.matchedAllocation = null;
+    this.matchedRule = null;
+    this.unmatchedAllocations = [];
+    this.unevaluatedAllocations = [];
+    return this;
+  };
+
+  setNoMatchFound = (
+    unmatchedAllocations: Array<AllocationEvaluation> = [],
+  ): FlagEvaluationDetailsBuilder => {
+    this.variationKey = null;
+    this.variationValue = null;
+    this.matchedAllocation = null;
+    this.matchedRule = null;
+    this.unmatchedAllocations = unmatchedAllocations;
+    this.unevaluatedAllocations = this.allocations.map(
+      (allocation, i) =>
+        ({
+          key: allocation.key,
+          allocationEvaluationCode: AllocationEvaluationCode.UNEVALUATED,
+          orderPosition: i,
+        } as AllocationEvaluation),
+    );
+    return this;
+  };
+
+  setMatch = (
+    orderPosition: number,
+    variation: Variation,
+    allocation: Allocation,
+    matchedRule: Rule | null,
+    unmatchedAllocations: Array<AllocationEvaluation>,
+  ): FlagEvaluationDetailsBuilder => {
+    this.variationKey = variation.key;
+    this.variationValue = variation.value;
+    this.matchedRule = matchedRule;
+    this.matchedAllocation = {
+      key: allocation.key,
+      allocationEvaluationCode: AllocationEvaluationCode.MATCH,
+      orderPosition,
+    };
+    this.unmatchedAllocations = unmatchedAllocations;
+    const unevaluatedStartIndex = orderPosition + 1;
+    this.unevaluatedAllocations = this.allocations.slice(unevaluatedStartIndex).map(
+      (allocation, i) =>
+        ({
+          key: allocation.key,
+          allocationEvaluationCode: AllocationEvaluationCode.UNEVALUATED,
+          orderPosition: unevaluatedStartIndex + i,
+        } as AllocationEvaluation),
+    );
+    return this;
+  };
+
+  buildForNoneResult = (
+    flagEvaluationCode: FlagEvaluationCode,
+    flagEvaluationDescription: string,
+  ): FlagEvaluationDetails => this.setNone().build(flagEvaluationCode, flagEvaluationDescription);
+
+  build = (
+    flagEvaluationCode: FlagEvaluationCode,
+    flagEvaluationDescription: string,
+  ): FlagEvaluationDetails => ({
+    flagEvaluationCode,
+    flagEvaluationDescription,
+    variationKey: this.variationKey,
+    variationValue: this.variationValue,
+    matchedRule: this.matchedRule,
+    matchedAllocation: this.matchedAllocation,
+    unmatchedAllocations: this.unmatchedAllocations,
+    unevaluatedAllocations: this.unevaluatedAllocations,
+  });
+}
